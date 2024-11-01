4a44b6ce94d127672aeb3d8bf7bfc97c670827ce
John Lloyd <92360974+GeneralCore2000@users.noreply.github.com>
2024-11-01 17:14:35 +0800

    Removed Switch case

--- Activity3/src/main/java/com/mycompany/activity3/Activity3.java
+++ Activity3/src/main/java/com/mycompany/activity3/Activity3.java
@@ -16,32 +16,5 @@ public class Activity3 {
         System.out.print("[C] Circle\n[R] Rectangle\n[T] Triangle\n>>: ");
         choice = in.next().toUpperCase().charAt(0);
 
-        switch (choice) {
-            case 'C':
-                System.out.print("Enter RADIUS >>: ");
-                radius = in.nextDouble();
-                computedArea = 3.14 * radius * radius;
-                System.out.println("The area of the circle is: " + computedArea);
-                System.err.println("Invalid Input!");
-                break;
-            case 'R':
-                System.out.print("Enter LENGTH >>: ");
-                length = in.nextDouble();
-                System.out.print("Enter WIDTH >>: ");
-                width = in.nextDouble();
-                computedArea = length * width;
-                System.out.println("The area of the rectangle is: " + computedArea);
-                break;
-            case 'T':
-                System.out.print("Enter BASE >>: ");
-                base = in.nextDouble();
-                System.out.print("Enter HEIGHT >>: ");
-                height = in.nextDouble();
-                computedArea = 0.5 * base * height;
-                System.out.println("The area of the trianglee is: " + computedArea);
-                break;
-            default:
-                System.err.println("Select only from the choices");
-        }
     }
 }
