fa4b2d33cfcf3264e4239c530c7f2262cf8455bd
John Lloyd <92360974+GeneralCore2000@users.noreply.github.com>
2024-11-25 14:46:13 +0800

    Added Activity 6

---
+++ Activity6/src/main/java/com/mycompany/activity6/Activity6.java
@@ -0,0 +1,32 @@
+package com.mycompany.activity6;
+
+import java.util.Scanner;
+
+public class Activity6 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int dividend = 0, divisor = 0, quotient = 0, remainder = 0;
+
+        System.out.print("Dividend\t:\t");
+        dividend = in.nextInt();
+        System.out.print("Divisor \t:\t");
+        divisor = in.nextInt();
+        System.out.println("---------------------------------------------");
+
+        if (divisor <= 0 || dividend <= 0) {
+            System.out.println("Zero/Negative number/s not accepted");
+        } else if (divisor > dividend) {
+            System.out.println("    Unable to Process");
+        } else {
+            for (int i = divisor; i <= dividend; i += divisor) {
+                quotient++;
+            }
+            System.out.println("Quotient\t:\t" + quotient);
+            if (quotient * divisor < dividend) {
+                remainder = dividend - (quotient * divisor);
+                System.out.println("Remainder\t:\t" + remainder);
+            }
+        }
+    }
+}
