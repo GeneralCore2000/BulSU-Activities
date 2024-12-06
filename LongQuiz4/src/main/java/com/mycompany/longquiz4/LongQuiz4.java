9bd2ca0b2f9c1d29086cb4f9de362ceb82675287
John Lloyd <92360974+GeneralCore2000@users.noreply.github.com>
2024-12-06 15:55:58 +0800

    Added LongQuiz4

---
+++ LongQuiz4/src/main/java/com/mycompany/longquiz4/LongQuiz4.java
@@ -0,0 +1,35 @@
+package com.mycompany.longquiz4;
+
+import java.util.Scanner;
+
+public class LongQuiz4 {
+
+    public static String daysOfWeek(int day) {
+        switch (day) {
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Monday";
+            case 4:
+                return "Tuesday";
+            case 5:
+                return "Monday";
+            case 6:
+                return "Tuesday";
+            case 7:
+                return "Tuesday";
+        }
+        return "Error";
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int day = 0;
+
+        System.out.print("Enter number >>: ");
+        day = in.nextInt();
+        System.out.println(daysOfWeek(day));
+    }
+}
